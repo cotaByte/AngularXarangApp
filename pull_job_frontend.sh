1adbbbde127ab946f4d6846d8d0852ee40bc9a79
palmaAvenger <minanajoverrafa@gmail.com>
2023-03-12 10:45:04 +0100

    add echo for separator in shell

--- pull_job_frontend.sh
+++ pull_job_frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-echo "actualizando repositorio frontend=>$(date)";
+echo "==========================================================================="
+echo "actualizando repositorio frontend=> $(date)";
 git pull -v --rebase
-echo "repositorio frontend actualizado =>$(date)";
-
+echo "repositorio frontend actualizado => $(date)";
+echo "==========================================================================="
